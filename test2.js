289269f7a7d7c7c16ac9d814b76b45c028311cf1
Nostradamos <n0stradamos@mail.ru>
2017-05-09 00:04:58 +0200

    started coding a generic create/find function. For the future, in the moment we will still stick to the old functions, until we are more or less feature complete

--- test2.js
+++ test2.js
@@ -1,17 +1,5 @@
-const plantJournal = require('./lib/pj');
+const _ = require('lodash');
 
-async function main() {
-  pj = new plantJournal('./test.sql');
-  await pj.connect();
-  await pj.Family.create({familyName: 'testFamily1'});
-  await pj.Family.create({familyName: 'testFamily2'});
-  await pj.Generation.create({familyId: 1, generationName: 'F1'});
-  await pj.Generation.create({familyId: 1, generationName: 'F2'});
-  await pj.Generation.create({familyId: 2, generationName: 'S1'});
-  await pj.Plant.create({generationId: 1, plantName: 'testPlant1'});
-  await pj.Plant.create({generationId: 1, plantName: 'testPlant2'});
-  await pj.Generation.create({familyId: 2, generationName: 'S2', generationParents: [1,2]});
-  await pj.disconnect();
-}
+let arr = "test";
 
-main();
+console.log(_.every(_.isInteger));
