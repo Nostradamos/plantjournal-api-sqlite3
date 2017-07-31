1fd9f68f10a66d6a629d2d12fd99bd919956f4c8
Nostradamos <n0stradamos@mail.ru>
2017-07-31 20:11:45 +0200

    did stuff for applyfilter, still wip

--- src/apply-filter.js
+++ src/apply-filter.js
@@ -4,14 +4,7 @@ const QueryUtils = require('./utils-query');
 const CONSTANTS = require('./constants');
 const logger = require('./logger');
 
-/**
- *
- * @param  {[type]} obj         [description]
- * @param  {[type]} squelExpr   [description]
- * @param  {[type]} depth       [description]
- * @param  {[type]} [type=null] [description]
- * @return {[type]}             [description]
- */
+
 function eachFilterObject(obj, squelExpr, depth, type=null) {
   let isArray = _.isArray(obj);
 
@@ -26,40 +19,50 @@ function eachFilterObject(obj, squelExpr, depth, type=null) {
   console.log(obj, depth, type);
 
   let attr, attrOptions, crit, critArgs, table;
+
+  // iterate over every element, if obj is array, key will be index and
+  // value the element. Otherwise normal key/value pair
   _.each(obj, function(value, key) {
+      // If we have an array, value/element has to be an object. Just use
+      // this function again on it
       if(isArray === true) {
-        return eachFilterObject(value, squelExpr, depth+1, type);
+          return eachFilterObject(value, squelExpr, depth+1, type);
       }
 
       [attr, attrOptions] = [key, value];
 
       console.log(attr, attrOptions);
 
+      // Handle boolean operators
       if(attr == '$and') {
         return eachFilterObject(attrOptions, squelExpr, depth+1, 'and');
       } else if(attr == '$or') {
         return eachFilterObject(attrOptions, squelExpr, depth+1, 'or');
       } else if(attr === '$and()') {
+        // $and() is a bit different, we want to create a subexpression
         let subSquelExpr = squel.expr()
         eachFilterObject(attrOptions, subSquelExpr, depth+1, 'and');
         applyCriteriaToExpression(squelExpr, subSquelExpr, [], 'and');
       } else if(attr === '$or()') {
+        // $or() is a bit different, we want to have a subexpression
         let subSquelExpr = squel.expr()
         eachFilterObject(attrOptions, subSquelExpr, depth+1, 'or');
         applyCriteriaToExpression(squelExpr, subSquelExpr, [], 'or');
       } else if(_.indexOf(allowedAttributes, attr) !== -1){
-
+        // Handle normal attributes
         console.log(attr);
         table = QueryUtils.getTableOfField(attr);
 
-        if(_.isInteger(attrOptions) || _.isString(attrOptions)) {
+        if(attr == 'generationParents') {
+          [crit, critArgs] = handleGenerationParents(attrOptions);
+        } else if(_.isInteger(attrOptions) || _.isString(attrOptions)) {
           crit = "?.? = ?";
           critArgs = [table, attr, attrOptions];
         }
 
         applyCriteriaToExpression(squelExpr, crit, critArgs, type);
-
       } else {
+        // No boolean operator nor attribute, something's stinky here
         throw new Error('Illegal attribute: ' + attr);
       }
   });
@@ -79,7 +82,6 @@ function applyFilter(query, allowedAttributes, criteria) {
   this.allowedAttributes = allowedAttributes;
   let squelExpr = squel.expr();
   eachFilterObject.bind(this, criteria.filter, squelExpr, 1)();
-
   query.where(squelExpr);
 }
 
