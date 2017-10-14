908183dc626bc352a1bbfe6e46909bb607365269
Nostradamos <n0stradamos@mail.ru>
2017-10-14 16:10:54 +0200

    Improved comments for child attributes translator.

--- src/apply-where/translate-operators-child-attributes.js
+++ src/apply-where/translate-operators-child-attributes.js
@@ -11,7 +11,8 @@ const UtilsExpression = require('../utils/utils-expression');
 const TranslateOperatorsRelational = require('./translate-operators-relational');
 
 /**
- * This class translates criterias for generationParents.
+ * This class translates criterias for generationParents and all other child
+ * attributes like familyGenerations, generationGenotypes...
  * For example if you have a criteria object like this:
  * ```
  * {
@@ -22,15 +23,16 @@ const TranslateOperatorsRelational = require('./translate-operators-relational')
  * }
  * ```
  * Then we would process the "$eq: [1,2]" stuff inside this class.
- * NOTE: This class only contains static methods, so you can't create
- * a new instance of this class. To call this class, just call
+ * NOTE: This class only contains static methods, so you can't create a new
+ * instance of this class. To call this class, just call
  * TranslateOperatorsRelational.translateAndApplyOperators().
  */
 class TranslateOperatorsChildAttributes extends TranslateOperatorsRelational {
     /**
-     * We can force set attribute (self.attr) to plantId, because we will only
-     * query this attribute. Also the the table we will query will always be
-     * generationParents.
+     * We need to the (destination) table and attribute, and the (source) table
+     * and attributes. They are different because we wont build our query
+     * against the family table for the `familyGenerations` attribute but for
+     * the generations table.
      * Besides that we need to redirect self.squelExpr to a new squel expression
      * because we want to build a subquery in the end. We also need another
      * squel expression to handle the having part of the new sub query.
@@ -47,8 +49,10 @@ class TranslateOperatorsChildAttributes extends TranslateOperatorsRelational {
     }
 
     /**
-     * Operator function handling equals. generationParents are equal if they
-     * contain the exact same parentIds and don't have any other parents in it.
+     * Operator function handling equals. The child attribute value will be
+     * represented as an array, so the child attribute is equal to another
+     * array if the contain the same elements and have the same count of
+     * elements.
      * NOTE: We can't simply use the method provided by
      * TranslateOperatorsRelational because of the way we store
      * the generationParents. Each parent is in a own row which we join to.
@@ -65,34 +69,33 @@ class TranslateOperatorsChildAttributes extends TranslateOperatorsRelational {
      *         Expression object
      */
     static operatorEquals(self, operatorOptions, crit) {
+        // With the in expression we make sure we only select values which are
+        // in operator options
         this.operatorIn(self, operatorOptions, crit);
 
+        // With the having expression we make sure that the count of the
+        // previously selected values equals our operatorOptions.length.
         let [critHaving, critHavingArgs] = UtilsExpression.
             createEqualsExpression(
-                self.table,
-                self.attr,
-                // In case operatorOptions.length is undefined, we most likely
-                // have a single integer, so we only want to find generations
-                // with exactly one parent.
-                operatorOptions.length || 1,
-                'count'
-            );
+                self.table, self.attr, operatorOptions.length || 1, 'count');
 
         logger.silly(this.name, '#operatorEquals()', critHaving, critHavingArgs);
 
         UtilsExpression.applyExpression(
             self.squelExprHaving, critHaving, critHavingArgs, self.type);
-
     }
 
     /**
      * Operator function for equals NOT ($neq). This operator lets you find
-     * generations where generationParent doesn't equal a set of plantIds.
-     * The are not equal if they contain more/other plantIds or parentIds or if
-     * they just don't consist of those parentIds.
-     * NOTE: Can't use the TranslateOperatorsRelational.operatorNotEquals()
-     * method because we need don't really do an not equals, but more an not in.
-     * Remind that we check against an array of plantIds/parentIds.
+     * records where the child attribute value array is different to the
+     * operator options value array.
+     * They are not equal if they have a different count of elements or if they
+     * consist of other values.
+     * NOTE: We can't use the TranslateOperatorsRelational.operatorNotEquals()
+     * method because we don't do an unequal operatoion on a column but on a
+     * whole query of another table. So we more do a not in operation combined
+     * with another query selecting all child attribute ids which have a
+     * different count of values.
      * @param  {Object} self
      *         Object containing information about this translation process
      * @param  {Integer|Integer[]} operatorOptions
@@ -104,20 +107,14 @@ class TranslateOperatorsChildAttributes extends TranslateOperatorsRelational {
      *         added to self.squelExpr.
      */
     static operatorNotEquals(self, operatorOptions, crit) {
-        // To get all generations not equaling, we also need to find those who
-        // have a different amount of parents. We can't to this in one query,
-        // so we need to build a subquery.
-
+        // To for example get all generations which don't have a specific set of
+        // parents, we need to find generations which have a different amount of
+        // parents. We can't to this in one query, so we need to build a
+        // subquery with an own expression.
         let [exprCountNotEqual, exprCountNotEqualArgs] = UtilsExpression
             .createNotEqualsExpression(
-                self.table,
-                self.attr,
-                operatorOptions.length || 1,
-                'count'
-            );
+                self.table, self.attr, operatorOptions.length || 1, 'count');
 
-        // queryCountUnequal will find all generationIds which have a different
-        // amount of parents
         let queryCountUnequal = squel.select()
             .from(self.table)
             .field(
@@ -133,36 +130,42 @@ class TranslateOperatorsChildAttributes extends TranslateOperatorsRelational {
 
         logger.silly(this.name, '#operatorNotEquals() queryCountUnequal', queryCountUnequal);
 
+        // Next we have to find all rows where we have values different to any
+        // of operatorOptions. For the generationParents child attribute this
+        // would mean any record where the parents plant id is unequal to any
+        // of operatorOptions.
         let [exprNotIn, exprNotInArgs] = UtilsExpression
             .createNotInExpression(
-                self.srcTable,
-                self.srcAttr,
-                operatorOptions
-            );
+                self.srcTable, self.srcAttr, operatorOptions);
 
-        // queryNotIn will find all generationIds which have a different
-        // parentId
         let queryNotIn = squel.select()
             .from(self.table)
-            .field(
-                Utils.explicitColumnRstr(
-                    self.table, self.srcAttr))
+            .field(Utils.explicitColumnRstr(self.table, self.srcAttr))
             .where(exprNotIn, ...exprNotInArgs).toString();
 
         logger.silly(this.name, '#operatorNotEquals() queryNotIn', queryNotIn);
 
+        // Now combine both queries with an UNION and build the final
+        // expression.
+        // NOTE: We can't use the .union function from squel because it's not
+        // compatible with sqlite3. Because of that we also wrap the queries
+        // in a squel.rstr() to not have paranthesis around the query.
+        // See https://github.com/hiddentao/squel/issues/335
         crit.crit = '? IN (? UNION ?)';
         crit.args = [
-            Utils.explicitColumnRstr(
-                self.srcTable, self.srcAttr),
-            squel.rstr(queryCountUnequal.toString()),
+            Utils.explicitColumnRstr(self.srcTable, self.srcAttr),
+            squel.rstr(queryCountUnequal),
             squel.rstr(queryNotIn)
         ];
     }
 
     /**
-     * This operator lets you check for generation which have AT LEAST this
-     * parents, but can have more and others too.
+     * This operator lets you find records where the child attribute array
+     * contains all elements of operatorOptions. The child attribute can also
+     * contain more different elements.
+     * To achieve this we select all child records which have any of operator
+     * options elements in it, and from the selected we only keep those where
+     * we had at least operatorOptions.length matches.
      * @param  {Object} self
      *         Object containing information about this translation process
      * @param  {Integer|Integer[]} operatorOptions
@@ -178,14 +181,7 @@ class TranslateOperatorsChildAttributes extends TranslateOperatorsRelational {
 
         let [critHaving, critHavingArgs] = UtilsExpression.
             createGreaterThanEqualExpression(
-                self.table,
-                self.attr,
-                // In case operatorOptions.length is undefined, we most likely
-                // have a single integer, so we only want to find generations
-                // with exactly one parent
-                operatorOptions.length || 1,
-                'count'
-            );
+                self.table, self.attr, operatorOptions.length || 1, 'count');
 
         logger.silly(this.name, '#operatorHas()', critHaving, critHavingArgs);
 
@@ -194,8 +190,11 @@ class TranslateOperatorsChildAttributes extends TranslateOperatorsRelational {
     }
 
     /**
-     * This operator lets you select generations which don't have a set of
-     * parents.
+     * This operator lets you select records where the child attribute doesn't
+     * contain any of the elements in operatorOptions.
+     * To achieve this behaviour we first select all records where any of the
+     * operatorOptions elements is in, and then we select all records where
+     * the id doesn't match with the previously selected.
      * @param  {Object} self
      *         Object containing information about this translation process
      * @param  {Integer|Integer[]} operatorOptions
@@ -221,17 +220,12 @@ class TranslateOperatorsChildAttributes extends TranslateOperatorsRelational {
             .field(fieldParentGenerationIdRstr)
             .where(
                 '? IN ?',
-                Utils.explicitColumnRstr(
-                    self.table,
-                    self.attr),
+                Utils.explicitColumnRstr(self.table, self.attr),
                 operatorOptions)
             .group(fieldParentGenerationIdRstr);
 
         crit.crit = '? NOT IN ?';
-        crit.args = [
-            fieldParentGenerationIdRstr,
-            subQuery
-        ];
+        crit.args = [fieldParentGenerationIdRstr, subQuery];
     }
 
     /**
@@ -265,9 +259,9 @@ class TranslateOperatorsChildAttributes extends TranslateOperatorsRelational {
     }
 
     /**
-     * Before we're done, we need to build a sub query which selects all
-     * generationIds where a plantIds/parentIds matches and maybe even
-     * a having clause.
+     * Before we're done, we need to build a sub query were we select from the
+     * child attributes destination table the id's we want to select in the
+     * source table.
      * @param  {Object} self
      *         Object containing information about this translation process
      */
@@ -280,33 +274,23 @@ class TranslateOperatorsChildAttributes extends TranslateOperatorsRelational {
 
         let subQuery = squel.select()
             .from(self.table)
-            .field(
-                Utils.explicitColumnRstr(
-                    self.table, self.srcAttr));
+            .field(Utils.explicitColumnRstr(self.table, self.srcAttr));
 
 
         if(!emptySquelExpr) subQuery.where(self.squelExpr);
 
         if(!emptySquelExprHaving) {
             subQuery
-                .group(
-                    Utils.explicitColumnRstr(
-                        self.table, self.srcAttr))
+                .group(Utils.explicitColumnRstr(self.table, self.srcAttr))
                 .having(self.squelExprHaving);
         }
 
-        logger.silly(
-            '#applyCriteriaFilter #translateAndApplyGenerationParentsOperators() ' +
-            'generationParents subQuery:', subQuery.toString());
+        logger.silly('#applyCriteriaFilter #translateAndApplyChildAttributesOperators() subQuery:', subQuery.toString());
 
         UtilsExpression.applyExpression(
             self.squelExprOld,
             '? IN ?',
-            [
-                Utils.explicitColumnRstr(
-                    self.srcTable, self.srcAttr),
-                subQuery
-            ],
+            [Utils.explicitColumnRstr(self.srcTable, self.srcAttr), subQuery],
             self.type
         );
     }
